7f3fa7d9c5dadaaca553dce1bc15ad62edd8afdb
brandon <contactbrandonye@gmail.com>
2020-09-26 16:40:24 -0400

    added more fields to signup form, users are now added to db when signed up

--- qtma2020/src/components/Firebase/firebase.js
+++ qtma2020/src/components/Firebase/firebase.js
@@ -1,35 +1,60 @@
-import app from "firebase/app";
-import "firebase/auth";
+import app from 'firebase/app';
+import firebase from 'firebase';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyAIDEQxJbKMW2khU0ttLVaZ04TrU6vqCaM",
-  authDomain: "qtma2020.firebaseapp.com",
-  databaseURL: "https://qtma2020.firebaseio.com",
-  projectId: "qtma2020",
-  storageBucket: "qtma2020.appspot.com",
-  messagingSenderId: "107809599995",
-  appId: "1:107809599995:web:827f00396f098e60ec9b5e",
-  measurementId: "G-1TLTH2WVMK",
+	apiKey: 'AIzaSyAIDEQxJbKMW2khU0ttLVaZ04TrU6vqCaM',
+	authDomain: 'qtma2020.firebaseapp.com',
+	databaseURL: 'https://qtma2020.firebaseio.com',
+	projectId: 'qtma2020',
+	storageBucket: 'qtma2020.appspot.com',
+	messagingSenderId: '107809599995',
+	appId: '1:107809599995:web:827f00396f098e60ec9b5e',
+	measurementId: 'G-1TLTH2WVMK'
 };
 
 class Firebase {
-  constructor() {
-    app.initializeApp(config);
+	constructor() {
+		app.initializeApp(config);
 
-    this.auth = app.auth();
-  }
+		this.auth = app.auth();
+	}
 
-  emailSignUp = (email, password) =>
-    this.auth.createUserWithEmailAndPassword(email, password);
+	emailSignUp = (email, password) =>
+		this.auth.createUserWithEmailAndPassword(email, password);
 
-  emailSignIn = (email, password) =>
-    this.auth.signInWithEmailAndPassword(email, password);
+	emailSignIn = (email, password) =>
+		this.auth.signInWithEmailAndPassword(email, password);
 
-  signOut = () => this.auth.signOut();
+	signOut = () => this.auth.signOut();
 
-  resetPWD = (email) => this.auth.sendPasswordResetEmail(email);
+	resetPWD = (email) => this.auth.sendPasswordResetEmail(email);
 
-  updatePWD = (password) => this.auth.currentUser.updatePassword(password);
+	updatePWD = (password) => this.auth.currentUser.updatePassword(password);
+
+	addUser = (
+		firstName,
+		lastName,
+		email,
+		birthDate,
+		location,
+		education,
+		password
+	) => {
+		const db = firebase.firestore();
+		db.settings({
+			timestampsInSnapshots: true
+		});
+		const userRef = db.collection('users').add({
+			firstName,
+			lastName,
+			email,
+			birthDate,
+			location,
+			education,
+			password
+		});
+	};
 }
 
 export default Firebase;
--- qtma2020/src/components/Signup/index.js
+++ qtma2020/src/components/Signup/index.js
@@ -1,104 +1,212 @@
-import React, { Component } from "react";
-import { Link, withRouter } from "react-router-dom";
-import { compose } from "recompose";
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
-import { withFirebase } from "../Firebase";
-import * as ROUTES from "../../constants/routes";
-import Button from "@material-ui/core/Button";
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+
+import { Button, TextField, MenuItem, Grid } from '@material-ui/core';
+
+import {
+	MuiPickersUtilsProvider,
+	KeyboardDatePicker
+} from '@material-ui/pickers';
 
 const SignupPage = () => (
-  <div>
-    <h1>Welcome! Let's get you signed up.</h1>
-    <SignupForm />
-  </div>
+	<div>
+		<h1>Welcome! Let's get you signed up.</h1>
+		<SignupForm />
+	</div>
 );
 
 const INITIAL_STATE = {
-  email: "",
-  passwordOne: "",
-  passwordTwo: "",
-  error: null,
+	firstName: '',
+	lastName: '',
+	birthDate: new Date(),
+	location: '',
+	education: '',
+	email: '',
+	passwordOne: '',
+	passwordTwo: '',
+	error: null,
+	locations: ['Kingston, ON', 'Toronto, ON', 'Ottawa, ON', 'Vancouver, BC'],
+	educations: ['High School Student', 'University Student', 'Other']
 };
 
 class SignupFormBase extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
+		this.state = { ...INITIAL_STATE };
+	}
 
-    this.state = { ...INITIAL_STATE };
-  }
+	onSubmit = (event) => {
+		const {
+			firstName,
+			lastName,
+			email,
+			birthDate,
+			location,
+			education,
+			passwordOne
+		} = this.state;
 
-  onSubmit = (event) => {
-    const { email, passwordOne } = this.state;
+		// Add firebase auth + add user to db
+		this.props.firebase
+			.emailSignUp(email, passwordOne)
+			.then((authUser) => {
+				this.props.firebase.addUser(
+					firstName,
+					lastName,
+					email,
+					birthDate,
+					location,
+					education,
+					passwordOne
+				);
+				this.setState({ ...INITIAL_STATE });
+				this.props.history.push(ROUTES.HOME);
+			})
+			.catch((error) => {
+				this.setState({ error });
+			});
 
-    this.props.firebase
-      .emailSignUp(email, passwordOne)
-      .then((authUser) => {
-        this.setState({ ...INITIAL_STATE });
-        this.props.history.push(ROUTES.HOME);
-      })
-      .catch((error) => {
-        this.setState({ error });
-      });
+		event.preventDefault();
+	};
 
-    event.preventDefault();
-  };
+	onChange = (event) => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
 
-  onChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+	onDateChange = (date) => {
+		this.setState({
+			birthDate: date
+		});
+	};
 
-  render() {
-    const { username, email, passwordOne, passwordTwo, error } = this.state;
+	render() {
+		const {
+			username,
+			email,
+			passwordOne,
+			passwordTwo,
+			location,
+			education,
+			error
+		} = this.state;
 
-    const isInvalid =
-      passwordOne !== passwordTwo ||
-      passwordOne === "" ||
-      email === "" ||
-      username === "";
+		const isInvalid =
+			passwordOne !== passwordTwo ||
+			passwordOne === '' ||
+			email === '' ||
+			username === '' ||
+			location === '' ||
+			education === '';
 
-    return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="username"
-          value={username}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Full Name"
-        />
-        <br />
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <br />
-        <input
-          name="passwordOne"
-          value={passwordOne}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />
-        <br />
-        <input
-          name="passwordTwo"
-          value={passwordTwo}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Confirm Password"
-        />
-        <br />
-        <br />
-        <Button disabled={isInvalid} type="submit">
-          Sign Up
-        </Button>
-
-        {error && <p>{error.message}</p>}
-      </form>
-    );
-  }
+		return (
+			<form onSubmit={this.onSubmit}>
+				<TextField
+					style={{ width: '11em' }}
+					required
+					name='firstName'
+					label='First Name'
+					onChange={this.onChange}
+				/>
+				<br />
+				<TextField
+					style={{ width: '11em' }}
+					required
+					name='lastName'
+					label='Last Name'
+					onChange={this.onChange}
+				/>
+				<br />
+				<TextField
+					style={{ width: '11em' }}
+					required
+					name='email'
+					label='Email Address'
+					onChange={this.onChange}
+					type='email'
+				/>
+				<br />
+				<TextField
+					defaultValue=''
+					style={{ width: '11em' }}
+					required
+					name='location'
+					select
+					label='Location'
+					onChange={this.onChange}
+				>
+					{this.state.locations.map((option) => (
+						<MenuItem key={option} value={option}>
+							{option}
+						</MenuItem>
+					))}
+				</TextField>
+				<br />
+				<TextField
+					defaultValue=''
+					style={{ width: '11em' }}
+					name='education'
+					required
+					select
+					label='Education'
+					onChange={this.onChange}
+				>
+					{this.state.educations.map((option) => (
+						<MenuItem key={option} value={option}>
+							{option}
+						</MenuItem>
+					))}
+				</TextField>
+				<br />
+				<MuiPickersUtilsProvider utils={DateFnsUtils}>
+					<Grid container justify='space-around'>
+						<KeyboardDatePicker
+							style={{ width: '11em' }}
+							required
+							disableToolbar
+							variant='inline'
+							format='MM/dd/yyyy'
+							margin='normal'
+							name='birthDate'
+							label='Date of Birth'
+							value={this.state.birthDate}
+							onChange={this.onDateChange}
+							KeyboardButtonProps={{
+								'aria-label': 'change date'
+							}}
+						/>
+					</Grid>
+				</MuiPickersUtilsProvider>
+				<br />
+				<TextField
+					required
+					name='passwordOne'
+					label='Password'
+					type='password'
+					onChange={this.onChange}
+				/>
+				<br />
+				<TextField
+					required
+					name='passwordTwo'
+					label='Confirm password'
+					type='password'
+					onChange={this.onChange}
+				/>
+				<br />
+				<Button disabled={isInvalid} type='submit'>
+					Sign Up
+				</Button>
+				{error && <p>{error.message}</p>}
+			</form>
+		);
+	}
 }
 
 const SignupForm = compose(withRouter, withFirebase)(SignupFormBase);
