6c98ddc6870e2b6c78fc60f995d02e4aafb353b3
Brandon Ye <57378354+yebrandon@users.noreply.github.com>
2021-01-13 14:39:29 -0500

    added stepsblock component folder

---
+++ qtma2020/src/components/StepsBlock/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+function StepsBlock() {
+	return <div></div>;
+}
+
+export default StepsBlock;
+
+// Import into another file by writing "import StepsBlock from '../StepsBlock';" at the top of the file, then declaring it in the return function as <StepsBlock></StepsBlock>
