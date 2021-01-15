361f00f44af8a7ad8205c2bebf0ff1f5a617af88
yebrandon <contactbrandonye@gmail.com>
2021-01-15 17:33:36 -0500

    passing props from navbar to sign up dialog example

--- qtma2020/src/components/SignUpDialog/index.js
+++ qtma2020/src/components/SignUpDialog/index.js
@@ -1,263 +1,268 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import { compose } from "recompose";
-import { withFirebase } from "../Firebase";
-import * as ROUTES from "../../constants/routes";
-import "./index.css";
-import { FcGoogle } from "react-icons/fc";
-import { FaFacebook } from "react-icons/fa";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose, withProps } from 'recompose';
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+import './index.css';
+import { FcGoogle } from 'react-icons/fc';
+import { FaFacebook } from 'react-icons/fa';
 
 import {
-  Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  TextField,
-} from "@material-ui/core";
+	Button,
+	Dialog,
+	DialogTitle,
+	DialogContent,
+	DialogContentText,
+	TextField
+} from '@material-ui/core';
 
-const SignUpDialog = () => (
-  <div>
-    <SignUpButton />
-  </div>
+const SignUpDialog = (props) => (
+	<div>
+		<SignUpButton fontColor={props.fontColor} />
+	</div>
 );
 
 const INITIAL_STATE = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  passwordOne: "",
-  passwordTwo: "",
-  open: false,
-  error: null,
+	firstName: '',
+	lastName: '',
+	email: '',
+	passwordOne: '',
+	passwordTwo: '',
+	open: false,
+	error: null
 };
 
 class SignUpButtonBase extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...INITIAL_STATE };
-  }
+	constructor(props) {
+		super(props);
+		this.state = { ...INITIAL_STATE };
+	}
 
-  handleOpen = () => {
-    this.setState({ open: true });
-  };
+	handleOpen = () => {
+		this.setState({ open: true });
+		console.log(this.props);
+	};
 
-  handleClose = () => {
-    this.setState({ open: false });
-  };
+	handleClose = () => {
+		this.setState({ open: false });
+	};
 
-  onSubmit = (event) => {
-    const { firstName, lastName, email, passwordOne } = this.state;
+	onSubmit = (event) => {
+		const { firstName, lastName, email, passwordOne } = this.state;
 
-    // Add firebase auth + add user to db
-    this.props.firebase
-      .emailSignUp(email, passwordOne)
-      .then((authUser) => {
-        return this.props.firebase.addUser(firstName, lastName, email);
-      })
-      .then(() => {
-        return this.props.firebase.doSendEmailVerification();
-      })
-      .then((authUser) => {
-        this.setState({ ...INITIAL_STATE });
-        this.props.history.push(ROUTES.HOME);
-      })
-      .catch((error) => {
-        this.setState({ error });
-      });
+		// Add firebase auth + add user to db
+		this.props.firebase
+			.emailSignUp(email, passwordOne)
+			.then((authUser) => {
+				return this.props.firebase.addUser(firstName, lastName, email);
+			})
+			.then(() => {
+				return this.props.firebase.doSendEmailVerification();
+			})
+			.then((authUser) => {
+				this.setState({ ...INITIAL_STATE });
+				this.props.history.push(ROUTES.HOME);
+			})
+			.catch((error) => {
+				this.setState({ error });
+			});
 
-    event.preventDefault();
-    this.handleClose();
-    console.log(this.state);
-  };
+		event.preventDefault();
+		this.handleClose();
+		console.log(this.state);
+	};
 
-  onChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+	onChange = (event) => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
 
-  render() {
-    const {
-      firstName,
-      lastName,
-      email,
-      passwordOne,
-      passwordTwo,
-      error,
-    } = this.state;
+	render() {
+		const {
+			firstName,
+			lastName,
+			email,
+			passwordOne,
+			passwordTwo,
+			error
+		} = this.state;
 
-    const isInvalid =
-      passwordOne !== passwordTwo ||
-      passwordOne === "" ||
-      email === "" ||
-      firstName === "" ||
-      lastName === "";
+		const isInvalid =
+			passwordOne !== passwordTwo ||
+			passwordOne === '' ||
+			email === '' ||
+			firstName === '' ||
+			lastName === '';
 
-    return (
-      <div>
-        <button
-          //variant='outlined'
-          className="signupButton"
-          style={{
-            color: "#1B4843",
-            background: "#F3FDFC",
-            fontWeight: "500",
-            fontSize: "14px",
-            borderRadius: "50px",
-            padding: "8px 20px",
-          }}
-          //color='#FFFFFF'
-          onClick={this.handleOpen}
-        >
-          Get Started
-        </button>
+		return (
+			<div>
+				<button
+					//variant='outlined'
+					className='signupButton'
+					style={{
+						color: '#1B4843',
+						background: '#F3FDFC',
+						fontWeight: '500',
+						fontSize: '14px',
+						borderRadius: '50px',
+						padding: '8px 20px'
+					}}
+					//color='#FFFFFF'
+					onClick={this.handleOpen}
+				>
+					Get Started
+				</button>
 
-        <Dialog
-          maxWidth={false}
-          onClose={this.handleClose}
-          open={this.state.open}
-        >
-          <div className="container">
-            <div className="row">
-              <div className="columnSml">
-                <img className="dialogImg" src="7EB9B2.png" />
-              </div>
-              <div className="columnLrg">
-                <DialogTitle style={{ padding: "0rem 2rem" }}>
-                  Create an account
-                </DialogTitle>
-                <DialogContent style={{ padding: "0rem 2rem" }}>
-                  <DialogContentText style={{ paddingBottom: "1rem" }}>
-                    Fill in the information below to get started today!
-                  </DialogContentText>
-                  <form onSubmit={this.onSubmit}>
-                    <div className="row">
-                      <TextField
-                        fullWidth
-                        style={{ marginRight: "1rem" }}
-                        variant="outlined"
-                        InputLabelProps={{
-                          shrink: true,
-                        }}
-                        required
-                        name="firstName"
-                        label="First Name"
-                        placeholder="ex. John"
-                        onChange={this.onChange}
-                      />
-                      <TextField
-                        fullWidth
-                        style={{ marginLeft: "1rem" }}
-                        variant="outlined"
-                        InputLabelProps={{
-                          shrink: true,
-                        }}
-                        required
-                        name="lastName"
-                        label="Last Name"
-                        placeholder="Smith"
-                        onChange={this.onChange}
-                      />
-                    </div>
-                    <TextField
-                      fullWidth
-                      variant="outlined"
-                      InputLabelProps={{
-                        shrink: true,
-                      }}
-                      required
-                      name="email"
-                      label="Email Address"
-                      type="email"
-                      placeholder="john.smith@email.com"
-                      onChange={this.onChange}
-                    />
-                    <TextField
-                      fullWidth
-                      variant="outlined"
-                      InputLabelProps={{
-                        shrink: true,
-                      }}
-                      required
-                      name="passwordOne"
-                      label="Password"
-                      type="password"
-                      placeholder="Type password here"
-                      onChange={this.onChange}
-                    />
-                    <TextField
-                      fullWidth
-                      variant="outlined"
-                      InputLabelProps={{
-                        shrink: true,
-                      }}
-                      required
-                      name="passwordTwo"
-                      label="Confirm password"
-                      type="password"
-                      placeholder="Retype password"
-                      onChange={this.onChange}
-                    />
+				<Dialog
+					maxWidth={false}
+					onClose={this.handleClose}
+					open={this.state.open}
+				>
+					<div className='container'>
+						<div className='row'>
+							<div className='columnSml'>
+								<img className='dialogImg' src='7EB9B2.png' />
+							</div>
+							<div className='columnLrg'>
+								<DialogTitle style={{ padding: '0rem 2rem' }}>
+									Create an account
+								</DialogTitle>
+								<DialogContent style={{ padding: '0rem 2rem' }}>
+									<DialogContentText
+										style={{ paddingBottom: '1rem' }}
+									>
+										Fill in the information below to get
+										started today!
+									</DialogContentText>
+									<form onSubmit={this.onSubmit}>
+										<div className='row'>
+											<TextField
+												fullWidth
+												style={{ marginRight: '1rem' }}
+												variant='outlined'
+												InputLabelProps={{
+													shrink: true
+												}}
+												required
+												name='firstName'
+												label='First Name'
+												placeholder='ex. John'
+												onChange={this.onChange}
+											/>
+											<TextField
+												fullWidth
+												style={{ marginLeft: '1rem' }}
+												variant='outlined'
+												InputLabelProps={{
+													shrink: true
+												}}
+												required
+												name='lastName'
+												label='Last Name'
+												placeholder='Smith'
+												onChange={this.onChange}
+											/>
+										</div>
+										<TextField
+											fullWidth
+											variant='outlined'
+											InputLabelProps={{
+												shrink: true
+											}}
+											required
+											name='email'
+											label='Email Address'
+											type='email'
+											placeholder='john.smith@email.com'
+											onChange={this.onChange}
+										/>
+										<TextField
+											fullWidth
+											variant='outlined'
+											InputLabelProps={{
+												shrink: true
+											}}
+											required
+											name='passwordOne'
+											label='Password'
+											type='password'
+											placeholder='Type password here'
+											onChange={this.onChange}
+										/>
+										<TextField
+											fullWidth
+											variant='outlined'
+											InputLabelProps={{
+												shrink: true
+											}}
+											required
+											name='passwordTwo'
+											label='Confirm password'
+											type='password'
+											placeholder='Retype password'
+											onChange={this.onChange}
+										/>
 
-                    <div className="separator">or</div>
-                    <div className="row">
-                      <Button
-                        startIcon={<FcGoogle />}
-                        fullWidth
-                        variant="outlined"
-                        style={{
-                          borderColor: "#BDBDBD",
-                          color: "#777777",
-                        }}
-                      >
-                        Continue with Google
-                      </Button>
-                    </div>
-                    <div className="row">
-                      <Button
-                        startIcon={
-                          <FaFacebook
-                            style={{
-                              color: "#1877f2",
-                            }}
-                          />
-                        }
-                        fullWidth
-                        variant="outlined"
-                        style={{
-                          borderColor: "#BDBDBD",
-                          color: "#777777",
-                        }}
-                      >
-                        Continue with Facebook
-                      </Button>
-                    </div>
-                    <div className="row">
-                      <p>
-                        Already have an account? <a href="">Log in</a>
-                      </p>
-                      <Button
-                        disabled={isInvalid}
-                        type="submit"
-                        variant="contained"
-                        style={{
-                          backgroundColor: "#437F55",
-                          borderRadius: "50px",
-                          color: "#FFFFFF",
-                        }}
-                      >
-                        Sign Up
-                      </Button>
-                      {error && <p>{error.message}</p>}
-                    </div>
-                  </form>
-                </DialogContent>
-              </div>
-            </div>
-          </div>
-        </Dialog>
-      </div>
-    );
-  }
+										<div className='separator'>or</div>
+										<div className='row'>
+											<Button
+												startIcon={<FcGoogle />}
+												fullWidth
+												variant='outlined'
+												style={{
+													borderColor: '#BDBDBD',
+													color: '#777777'
+												}}
+											>
+												Continue with Google
+											</Button>
+										</div>
+										<div className='row'>
+											<Button
+												startIcon={
+													<FaFacebook
+														style={{
+															color: '#1877f2'
+														}}
+													/>
+												}
+												fullWidth
+												variant='outlined'
+												style={{
+													borderColor: '#BDBDBD',
+													color: '#777777'
+												}}
+											>
+												Continue with Facebook
+											</Button>
+										</div>
+										<div className='row'>
+											<p>
+												Already have an account?{' '}
+												<a href=''>Log in</a>
+											</p>
+											<Button
+												disabled={isInvalid}
+												type='submit'
+												variant='contained'
+												style={{
+													backgroundColor: '#437F55',
+													borderRadius: '50px',
+													color: '#FFFFFF'
+												}}
+											>
+												Sign Up
+											</Button>
+											{error && <p>{error.message}</p>}
+										</div>
+									</form>
+								</DialogContent>
+							</div>
+						</div>
+					</div>
+				</Dialog>
+			</div>
+		);
+	}
 }
 
 const SignUpButton = compose(withRouter, withFirebase)(SignUpButtonBase);
